8bd9aa6e1a97c23b5e33525ec4feed866b2b8ed5
MarianoMSP <escision2002@gmail.com>
2022-03-31 23:30:14 -0300

    metiendo mano 1era vez

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT =
-  process.env.PORT ||
-  (3000, () => console.log("levantando servidor con express"));
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-//localhost:3000/
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
